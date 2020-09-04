d2efa8f62d1272170a3fe4ee250f9207270e81ab
Marina Pisareva <aniram1s@yandex.ru>
2020-09-04 21:57:42 +0300

    Updates

---
+++ src/components/app/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel'
+import PostStatusFilter from '../post-status-filter'
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+import PostAddFrom from '../post-add-form';
+
+import './app.css'
+
+const App = () => {
+    return(
+ <div className='App'>
+        <AppHeader/>
+    <div className='search-panel d-flex'>
+        <SearchPanel/>
+        <PostStatusFilter/>
+    </div>
+    <PostList/>
+    <PostAddFrom/>
+ </div>
+    ) 
+}
+
+export default App
---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PostAddFrom = () => {
+    return (
+        <form className='bottom=panel d-flex'>
+            <input
+            type='text'
+            placeholder='Напишите что-нибудь'
+            className='form-control new-post-label'
+            />
+        <button 
+        type='submit'
+        className='btn btn-outline-secondary'>
+        Добавить
+        </button>
+        </form>
+    )
+}
+export default PostAddFrom;
