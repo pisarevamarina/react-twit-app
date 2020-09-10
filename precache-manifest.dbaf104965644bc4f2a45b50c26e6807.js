2dfacdc950f01f4d39b36bd3a83b5e803c90bdd8
Marina Pisareva <aniram1s@yandex.ru>
2020-09-10 23:03:43 +0300

    Updates

---
+++ precache-manifest.dbaf104965644bc4f2a45b50c26e6807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b6ff587b2f3110df3d63ad8200360b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23df163933c6aa1d6cf7",
+    "url": "/static/css/main.a8c52496.chunk.css"
+  },
+  {
+    "revision": "2de53eab62faf0bf6c23",
+    "url": "/static/js/2.fa976148.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa976148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23df163933c6aa1d6cf7",
+    "url": "/static/js/main.8fee6dae.chunk.js"
+  },
+  {
+    "revision": "212f40fc2e5f1e7ff2cc",
+    "url": "/static/js/runtime-main.3fbf303e.js"
+  }
+]);
--- src/components/post-add-form/index.js
+++
@@ -1,2 +0,0 @@
-import PostAddFrom from './post-add-form';
-export default PostAddFrom;
