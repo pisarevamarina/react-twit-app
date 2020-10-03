901f89e059f0023613dda658a65289cde2e48beb
Marina Pisareva <aniram1s@yandex.ru>
2020-10-03 23:01:17 +0300

    сделала счетчик лайков

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,64 +8,89 @@ import PostAddForm from '../post-add-form';
 import PostAddFrom from '../post-add-form';
 import './app.css';
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [
-        { label: 'Осваиваю React!', important: true, id: 1 },
-        { label: 'Купить кофе', important: false, id: 2 },
-        { label: 'Поспать', important: false, id: 3 },
+        { label: 'Осваиваю React!', important: true, like: false, id: 1 },
+        { label: 'Купить кофе', important: false, like: false, id: 2 },
+        { label: 'Поспать', important: false, like: false, id: 3 },
       ],
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
 
     this.maxId = 4;
   }
 
   deleteItem(id) {
-this.setState(({data}) => {
-    const index = data.findIndex(elem => elem.id === id);
-    const before = data.slice(0, index);
-    const after = data.slice(index +1);
-    const newData = [...before, ...after];
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+      const newData = [...before, ...after];
 
-    return {
-        data: newData
-    }
-})
+      return {
+        data: newData,
+      };
+    });
   }
 
   addItem(body) {
-     const newItem = {
-         label: body,
-         important: false,
-         id: this.maxId++
-     }
+    const newItem = {
+      label: body,
+      important: false,
+      id: this.maxId++,
+    };
 
-    this.setState(({data}) =>{
-        const newData = [...data, newItem];
-        return {
+    this.setState(({ data }) => {
+      const newData = [...data, newItem];
+      return {
+        data: newData,
+      };
+    });
+  }
+
+  onToggleImportant(id) {
+      console.log(id)
+  }
+
+  onToggleLiked(id) {
+      this.setState(({data}) => {
+          const index = data.findIndex(elem => elem.id ===id);
+          const old = data[index];
+          const newItem = {...old, like: !old.like}
+          const before = data.slice(0, index);
+          const after = data.slice(index + 1);
+          const newData = [...before, newItem, ...after];
+        return{
             data: newData
         }
-    })
+      })
   }
 
   render() {
-    return(
-    <div className='app'>
-      <AppHeader />
-      <div className='search-panel d-flex'>
-        <SearchPanel />
-        <PostStatusFilter />
+      const {data} = this.state;
+
+      const liked = data.filter(item => item.like ).length;
+      const allPosts = data.length;
+    return (
+      <div className='app'>
+        <AppHeader liked={liked} allPosts={allPosts}/>
+        <div className='search-panel d-flex'>
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList
+         posts={this.state.data} 
+        onDelete={this.deleteItem}
+        onToggleImportant={this.onToggleImportant}
+        onToggleLiked={this.onToggleLiked}/>
+        <PostAddFrom onAdd={this.addItem} />
       </div>
-      <PostList 
-      posts={this.state.data} 
-      onDelete={this.deleteItem} />
-      <PostAddFrom 
-      onAdd={this.addItem}/>
-    </div>
-    )
+    );
   }
 }
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -2,32 +2,20 @@ import React from 'react';
 import './post-list-item.css';
 
 export default class PostListItem extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            important: false,
-            like: false
-        }
-this.onImportant = this.onImportant.bind(this);
-this.onLike = this.onLike.bind(this);
-
-    }
-
-  onImportant= (state) => {
+  onImportant = (state) => {
       this.setState({
           important: !this.state.important,
       })
   }
 
-  onLike= (state) => {
+  onLike = (state) => {
     this.setState({
         like: !this.state.like,
     })
 }
 
     render() {
-        const {label, onDelete} = this.props;
-        const {important, like} = this.state
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
             let classNames = 'app-list-item d-flex justify-content-between';
             if (important) {
                 classNames += ' important';
@@ -39,9 +27,9 @@ this.onLike = this.onLike.bind(this);
 
         return(
             <div className= {classNames}>
-            <span className='app-list-item-label' onClick={this.onLike}>{label}</span>
+            <span className='app-list-item-label' onClick={onToggleLiked}>{label}</span>
             <div className='d-flex justify-content-center align-items-center'>
-              <button type='button' className='btn-star btn-sm' onClick={this.onImportant}>
+              <button type='button' className='btn-star btn-sm' onClick={onToggleImportant}>
                 <i className='fa fa-star'></i>
               </button>
               <button type='button' className='btn-trash btn-sm' onClick={onDelete}>
